868f3daa3ac576d276863cf50ec57e07af7ac2f9
abdullahsafwan <abdullahsafwan768@gmail.com>
2026-01-11 20:33:07 +0500

    formatting

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ app.use(
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "Cache-Control"],
-  })
+  }),
 );
 
 app.use(express.json());
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import app from "./app";
 import config from "./config/env";
 
-const port = config.port
+const port = config.port;
 
-app.use('/', (req, res)=>{
-    res.send('Server is up and running')
-})
+app.use("/", (req, res) => {
+  res.send("Server is up and running");
+});
 
-app.listen(port, ()=>{
-    console.log('listening to port', port)
-})
+app.listen(port, () => {
+  console.log("listening to port", port);
+});
 
-export default app
+export default app;
